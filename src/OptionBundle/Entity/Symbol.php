c74740ee3bb79f23a315b9afffc81980b24cf883
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-02-06 18:52:40 +0300

    Добавлен SymbolEnum

--- src/OptionBundle/Entity/Symbol.php
+++ src/OptionBundle/Entity/Symbol.php
@@ -3,6 +3,7 @@
 namespace OptionBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use OptionBundle\Enum\SymbolEnum;
 
 /**
  * Symbol
@@ -12,58 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Symbol
 {
 
-    // Energies
-    const CRUDE_OIL_WTI = 'CL';
-    const ULSD_NY_HARBOR = 'HO';
-    const GASOLINE_RBOB = 'RB';
-    const NATURAL_GAS = 'NG';
-    const CRUDE_OIL_BRENT = 'QA';
-    const ETHANOL = 'ZK';
-
-    // Financials
-    const FED_FUNDS_30_DAY = 'ZQ';
-    const EURODOLLAR = 'GE';
-
-    // Grains
-    const WHEAT = 'ZW';
-    const CORN = 'ZC';
-    const SOYBEANS = 'ZS';
-    const SOYBEAN_MEAL = 'ZM';
-    const SOYBEAN_OIL = 'ZL';
-    const OATS = 'ZO';
-    const ROUGH_RICE = 'ZR';
-    const HARD_RED_WHEAT = 'KE';
-    const SPRING_WHEAT = 'MW';
-    const CANOLA = 'RS';
-
-    // Indices
-    const E_MINI_SNP_500 = 'ES';
-    const MINI_NASDAQ_100 = 'NQ';
-    const DJIA_MINI = 'YM';
-
-    // Meats
-    const LIVE_CATTLE = 'LE';
-    const FEEDER_CATTLE = 'GF';
-    const LEAN_HOGS = 'HE';
-    const CLASS_III_MILK = 'DL';
-
-    // Metals
-    const GOLD = 'GC';
-    const SILVER = 'SI';
-    const HIGH_GRADE_COPPER = 'HG';
-    const PLATINUM = 'PL';
-    const PALLADIUM = 'PA';
-
-    // Softs
-    const COTTON = 'CT';
-    const ORANGE_JUICE = 'OJ';
-    const COFFEE = 'KC';
-    const SUGAR = 'SB';
-    const COCOA = 'CC';
-    const LUMBER = 'LS';
-
     /**
-     * @var string
+     * @var SymbolEnum
      * @ORM\Column(name="symbol", type="string", length=2)
      * @ORM\Id
      */
@@ -76,9 +27,10 @@ class Symbol
     private $name;
 
     /**
-     * @param string $symbol
+     * @param SymbolEnum $symbol
+     * @param string     $name
      */
-    public function __construct(string $symbol, string $name)
+    public function __construct(SymbolEnum $symbol, string $name)
     {
         $this->symbol = $symbol;
         $this->name = $name;
@@ -86,9 +38,9 @@ class Symbol
 
     /**
      * Get symbol
-     * @return string
+     * @return SymbolEnum
      */
-    public function getSymbol(): string
+    public function getSymbol(): SymbolEnum
     {
         return $this->symbol;
     }
