f631bfe44c1151ef5cfe82983de737503cb332c7
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-02-07 18:48:25 +0300

    FuturesRepository::findOneBySymbolAndExpiration()

--- src/OptionBundle/Repository/FuturesRepository.php
+++ src/OptionBundle/Repository/FuturesRepository.php
@@ -2,9 +2,25 @@
 
 namespace OptionBundle\Repository;
 
+use OptionBundle\Entity\Futures;
+use OptionBundle\Entity\Symbol;
+
 /**
  * FuturesRepository
  */
 class FuturesRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    /**
+     * @param Symbol $symbol
+     * @param string $futuresExpirationDate
+     * @return Futures
+     */
+    public function findOneBySymbolAndExpiration(Symbol $symbol, string $futuresExpirationDate)
+    {
+        return $this->findOneBy([
+            'symbol'     => $symbol->getSymbol(),
+            'expiration' => new \DateTime($futuresExpirationDate),
+        ]);
+    }
 }
