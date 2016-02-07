f75489c2112762cc0814b2e2b5375062a95b877f
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-02-07 12:30:12 +0300

    Генерация url таблицы опционов по символу и месяцу

--- src/OptionBundle/Service/PriceCollector.php
+++ src/OptionBundle/Service/PriceCollector.php
@@ -6,17 +6,69 @@
 
 namespace OptionBundle\Service;
 
+use OptionBundle\Entity\Symbol;
 use OptionBundle\Exception\WrongMonth;
 
 class PriceCollector
 {
 
     /**
+     * @var \DateTime
+     */
+    private $dateTime;
+
+    /**
+     * @param \DateTime $dateTime
+     */
+    public function __construct(\DateTime $dateTime)
+    {
+
+        $this->dateTime = $dateTime;
+    }
+
+    /**
+     * @param Symbol $symbol
+     * @param int    $monthNumber
+     * @throws WrongMonth
+     * @return string
+     */
+    public function collectOptionPrices(Symbol $symbol, int $monthNumber): string
+    {
+        $optionTableUrl = $this->createOptionTableUrl($symbol, $monthNumber);
+    }
+
+    /**
+     * @param Symbol $symbol
+     * @param int    $monthNumber
+     * @return string
+     * @throws WrongMonth
+     */
+    private function createOptionTableUrl(Symbol $symbol, int $monthNumber): string
+    {
+        if ($monthNumber > 12) {
+            throw new WrongMonth();
+        }
+
+        $currentMonth = (int) $this->dateTime->format('n');
+        $currentYear = (int) $this->dateTime->format('Y');
+        $yearOfContract = ($currentMonth > $monthNumber) ? $currentYear + 1 : $currentYear;
+
+        $optionTableUrl = sprintf(
+            'http://www.barchart.com/commodityfutures/null/options/%s%s%d?view=split&mode=i',
+            $symbol->getSymbol(),
+            $this->getMonthLetter($monthNumber),
+            $yearOfContract
+        );
+
+        return $optionTableUrl;
+    }
+
+    /**
      * @param int $monthNumber
      * @return string
      * @throws WrongMonth
      */
-    public function getMonthLetter(int $monthNumber): string
+    private function getMonthLetter(int $monthNumber): string
     {
         $monthLetters = [
             1  => 'F',
