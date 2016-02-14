41a148d1cfd3dfbdb84544ce96335aa877626a7f
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-02-14 16:30:19 +0300

    Наследование шаблонов и контроллеры для отображения страниц

---
+++ src/OptionBundle/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Author: Vehsamrak
+ * Date Created: 14.02.16 16:17
+ */
+
+namespace OptionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends Controller
+{
+
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('OptionBundle:Default:index.html.twig', []);
+    }
+}
